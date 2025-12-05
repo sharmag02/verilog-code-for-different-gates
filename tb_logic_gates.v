aa3e9317a360b0debc4c0b8380ffa624ec162ad4
Gaurav Kumar Sharma <91082465+sharmag02@users.noreply.github.com>
2025-12-05 18:57:32 +0530

    Update tb_logic_gates.v

--- tb_logic_gates.v
+++ tb_logic_gates.v
@@ -1,7 +1,3 @@
-// ----------------------------------------
-// Testbench for Logic Gates
-// ----------------------------------------
-
 `timescale 1ns/1ps
 
 module tb_logic_gates;
@@ -9,7 +5,6 @@ module tb_logic_gates;
     reg a, b;
     wire y_and, y_or, y_nand, y_nor, y_xor, y_xnor, y_not;
 
-    // Instantiate modules
     and_gate  u1 (y_and,  a, b);
     or_gate   u2 (y_or,   a, b);
     not_gate  u3 (y_not,  a);
@@ -19,24 +14,12 @@ module tb_logic_gates;
     xnor_gate u7 (y_xnor, a, b);
 
     initial begin
-        $display("A B | AND OR NAND NOR XOR XNOR NOT(A)");
-
-        // Test all combinations of a and b
+        $display("A B | AND OR NAND NOR XOR XNOR NOT");
+        
         a=0; b=0; #10;
-        $display("%b %b |  %b   %b   %b    %b    %b    %b     %b", 
-                  a, b, y_and, y_or, y_nand, y_nor, y_xor, y_xnor, y_not);
-
         a=0; b=1; #10;
-        $display("%b %b |  %b   %b   %b    %b    %b    %b     %b", 
-                  a, b, y_and, y_or, y_nand, y_nor, y_xor, y_xnor, y_not);
-
         a=1; b=0; #10;
-        $display("%b %b |  %b   %b   %b    %b    %b    %b     %b", 
-                  a, b, y_and, y_or, y_nand, y_nor, y_xor, y_xnor, y_not);
-
         a=1; b=1; #10;
-        $display("%b %b |  %b   %b   %b    %b    %b    %b     %b", 
-                  a, b, y_and, y_or, y_nand, y_nor, y_xor, y_xnor, y_not);
 
         $finish;
     end
